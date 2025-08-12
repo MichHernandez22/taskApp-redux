8546ccff8e834c97754aa00c4298e19ddc9f6e3c
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-08-12 11:37:27 -0600

    edit task

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.css';
 import { useSelector } from 'react-redux';
 import TaskForm from './components/TaskForm';
 import TasksList from './components/TasksList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <TaskForm/>
-      <TasksList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TasksList/>}/>
+          <Route path="/create-task" element={<TaskForm/>} />
+          <Route path="/edit-task/:id" element={<TaskForm/>} />
+        </Routes>        
+      </BrowserRouter>      
     </div>
   );
 }
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux"
 import { deleteTask } from "../features/tasks/taskSlice"
+import { Link } from "react-router-dom"
 
 const TasksList = () => {
 
@@ -12,14 +13,18 @@ const TasksList = () => {
 
   return (
     <div>
+        <header>
+            <h1>Tasks {tasks.length}</h1>
+            <Link to='/create-task'>Create Task</Link>
+        </header>
     {
         tasks.map(task => (
             <div key={task.id}>
                 <h3>{task.title}</h3>
                 <p>{task.description}</p>
                 <button onClick={() => handleDelete(task.id)}>Delete</button>
-            </div>
-            
+                <Link to={`edit-task/${task.id}`}>Edit</Link>
+            </div>            
         ) )    
     }
     </div>
